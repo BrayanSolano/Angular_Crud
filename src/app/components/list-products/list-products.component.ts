83a8c7c7c50535af765eed39b8cbf3d29bd1be85
BrayanSolano <brayansolanosanchez@gmail.com>
2025-04-13 10:23:26 -0500

    Creacion del Front, componentes, servicios y demas

---
+++ src/app/components/list-products/list-products.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {Product} from '../../interfaces/product';
+import { OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-products',
+  standalone: false,
+  templateUrl: './list-products.component.html',
+  styleUrl: './list-products.component.css'
+})
+export class ListProductsComponent implements OnInit {
+  listProducts: Product[] = [
+    {name: 'Coca cola', description: 'Bebida gaseosa', price: 2, stock: 10},
+    {name: 'Corona', description: 'Bebida con Alcohol', price: 5, stock: 20}
+  ]
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
