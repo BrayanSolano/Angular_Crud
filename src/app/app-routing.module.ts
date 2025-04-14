df4f0f11e00a52fd65c9978667c0f5cd701c6e77
BrayanSolano <brayansolanosanchez@gmail.com>
2025-04-13 20:55:01 -0500

    configuracion de rutas y agregar botones de editar y delete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//componentes
+import { ListProductsComponent } from './components/list-products/list-products.component';
+import { AddEditProductComponent } from './components/add-edit-product/add-edit-product.component';
+
+
+const routes: Routes = [
+  {path: '', component: ListProductsComponent},
+  {path: 'add', component: AddEditProductComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'} // redirecciona a la lista de productos si la ruta no existe
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
