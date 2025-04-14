a8e7243bb1292a4f0dba8d659910883d4f49edff
BrayanSolano <brayansolanosanchez@gmail.com>
2025-04-13 22:09:14 -0500

    importamos ractive form y enlazamos el formulario

--- src/app/components/add-edit-product/add-edit-product.component.ts
+++ src/app/components/add-edit-product/add-edit-product.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-edit-product',
@@ -6,6 +7,22 @@ import { Component } from '@angular/core';
   templateUrl: './add-edit-product.component.html',
   styleUrl: './add-edit-product.component.css'
 })
-export class AddEditProductComponent {
+export class AddEditProductComponent implements OnInit {
+  
+  form: FormGroup;
+  
+  constructor(private fb: FormBuilder) { 
+    this.form= this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      price: ['null', Validators.required],
+      stock: ['null', Validators.required],
+    });
+  }
+
+
+  ngOnInit(): void {
+    // Initialization logic here
+  }
 
 }
